3b890ab4f0f6d86b07b2e5a803b5b031fa3fef0d
KlugeClump <klugeclump@hotmail.com>
2016-11-04 17:15:42 -0500

    Modified Service
    Started Playing around with the Controllers/Models, Added a Testing
    solution, still need to wire in the MySQL Database

---
+++ JunkBoxService/JunkBox/Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using JunkBox.Models;
+
+namespace JunkBox.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        Address mockAddress = new Address { };
+        //Mock Database
+        Customer[] customers = new Customer[]
+        {
+            new Customer { Id = 1, FirstName = "Gale", LastName = "Smith", Email = "gale@mail.com", BillingAddress = new Address(), ShippingAddress = new Address()},
+            new Customer { Id = 2, FirstName = "Walter", LastName = "Cronkite", Email = "news@google.com", BillingAddress = new Address(), ShippingAddress = new Address()},
+            new Customer { Id = 3, FirstName = "SuperDude", LastName = "CodeWizard", Email= "it@codewriter.org", BillingAddress = new Address(), ShippingAddress = new Address()},
+        };
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return customers;
+        }
+
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault((p) => p.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        //Does not work yet
+        public IHttpActionResult GetCustomerByFirstName(string firstName)
+        {
+            var customer = customers.FirstOrDefault((p) => p.FirstName == firstName);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
--- JunkBoxService/JunkBoxService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JunkBoxService.WebApiApplication" Language="C#" %>
