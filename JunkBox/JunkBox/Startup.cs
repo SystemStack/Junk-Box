2437ba51f73c0d05151203ac2773ac86c4906c40
KlugeClump <klugeclump@hotmail.com>
2016-11-12 15:27:50 -0600

    MVC Update
    Updated Project to begin using MCV with API instead of only API

---
+++ JunkBox/JunkBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JunkBox.Startup))]
+namespace JunkBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
