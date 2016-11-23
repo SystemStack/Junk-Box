a1378e43bacb6913943bbe2415560dd82569f845
KlugeClump <klugeclump@hotmail.com>
2016-11-23 03:16:03 -0600

    More Ebay
    Added stuff to communicate with Ebay through C# instead of just through
    AngularJS

---
+++ JunkBox/JunkBox/Controllers/EbayController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using JunkBox.Models;
+using JunkBox.Common;
+
+namespace JunkBox.Controllers
+{
+    public class EbayController : Controller
+    {
+
+        //POST: Ebay/GetSomething/{data}
+        [HttpPost]
+        public ActionResult GetSomething(EbayGetSomethingModel data)
+        {
+            return Json(new { result=Ebay.GetTimestamp() });
+        }
+    }
+}
