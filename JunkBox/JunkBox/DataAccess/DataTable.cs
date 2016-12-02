35fdf6df71f240fc37d35524cb05e2d4dc5d7491
KlugeClump <klugeclump@hotmail.com>
2016-12-01 21:58:18 -0600

    update
    this update was left behind, I guess?

---
+++ JunkBox/JunkBox/DataAccess/DataTable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using JunkBox.Models;
+
+namespace JunkBox.DataAccess
+{
+    public abstract class DataTable
+    {
+        protected static IDataAccess dataAccess = MySqlDataAccess.GetDataAccess();
+
+        protected DataTable()
+        {
+
+        }
+
+        protected NonQueryResultModel PrepareNonQueryResult(int result, int expectedResult = 1)
+        {
+            bool succeeded = false;
+            if (result == expectedResult)
+            {
+                succeeded = true;
+            }
+
+            NonQueryResultModel payload = new NonQueryResultModel()
+            {
+                Success = succeeded
+            };
+
+            return payload;
+        }
+    }
+}
